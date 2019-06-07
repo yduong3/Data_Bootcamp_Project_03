1d3d2490f45fa9a8096f721fdd397ba6698cc4ac
Avis Sokol <46509535+Vangroovy@users.noreply.github.com>
2019-06-06 21:11:09 -0400

    wards route
    the wards route pushes data that initiates the js.

--- Toronto_Ward_Project/app.py
+++ Toronto_Ward_Project/app.py
@@ -20,6 +20,8 @@ app = Flask(__name__)
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db/to_ward_project.sqlite"
 db = SQLAlchemy(app)
+engine = create_engine("sqlite:///db/to_ward_project.sqlite")
+session = Session(engine)
 
 # reflect an existing database into a new model
 Base = automap_base()
@@ -32,15 +34,43 @@ crime2018 = Base.classes.Crime2018
 
 
 @app.route("/")
-def index():
+def characteristics():
     """Return the homepage."""
-    return render_template("index.html")
+    return render_template("homepage.html")
 
+@app.route("/wards")
+def wards_data():
+    # return the ward characteristic data
+    ward = db.session.query(wards.Ward, 
+                            wards.Owned_2016,
+                            wards.Rental_2016,
+                            wards.hh_with_children_2016,
+                            wards.Owned_2011,
+                            wards.Rental_2011,
+                            wards.hh_with_children_2011
+                            ).\
+                            order_by(wards.Ward).all()
+    warddata = []
+    plot_dict = {
+        'wards': [],
+        'owned_2016': [],
+        'rental_2016': [],
+        'hh_with_children_2016': [],
+        'owned_2011': [],
+        'rental_2011': [],
+        'hh_with_children_2011': [],
+    }
+    for row in ward:
+        plot_dict['wards'].append(row.Ward)
+        plot_dict['owned_2016'].append(row.Owned_2016)
+        plot_dict['rental_2016'].append(row.Rental_2016)
+        plot_dict['hh_with_children_2016'].append(row.hh_with_children_2016)
+        plot_dict['owned_2011'].append(row.Owned_2011)
+        plot_dict['rental_2011'].append(row.Rental_2011)
+        plot_dict['hh_with_children_2011'].append(row.hh_with_children_2011)
+    print(warddata)
+    return jsonify(plot_dict)
 
-@app.route("/income")
-def income():
-
-    return jsonify()
 
 if __name__ == "__main__":
     app.run()
