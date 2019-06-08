ceec099f812e04870ec49ab99e9bd1bfe25ead38
Avis Sokol <46509535+Vangroovy@users.noreply.github.com>
2019-06-08 11:03:30 -0400

    overlays

--- Toronto_Ward_Project_R3/toronto_gentrification/static/js/characteristics.js
+++ Toronto_Ward_Project_R3/toronto_gentrification/static/js/characteristics.js
@@ -13,81 +13,104 @@ d3.json("/wards", function(values) {
             color: "blue",
             symbol: "square",
             size: 10
-            },
+            },            
     };
+    var trace4 = {
+        x: dataset.ward,
+        y: dataset.hh_with_children_2011,
+        name: "2011 Households with Children",
+        type: "scatter",
+        mode: "markers",
+        marker: {
+            color: "red",
+            symbol: "circle",
+            size: 10
+            },            
+    }
 
-    var data = [trace3];
+    var data = [trace3, trace4];
     var layout = {
         xaxis: { title: "Ward" },
         yaxis: { title: "Characteristic"},
-        title: "2016 Households with Children"
+        title: "Households with Children"
     };
     Plotly.newPlot("plot", data, layout);
 });
 
 function getData(values){
-    var datay = [];
-    var color = [];
-    var symbol = [];
-    var name = [];
+    var datay1 = [];
+    var datay2 = [];
+    var color1 = [];
+    var color2 = [];
+    var symbol1 = [];
+    var symbol2 = [];
+    var name1 = [];
+    var name2 = [];
     //fill the array as a function of the selected dataset
     switch (values) {
-        case "dataset6":
-                datay = dataset.owned_2011;
-                color = "green";
-                symbol = "diamond";
-                name = "2011 Households with Owned Homes";
-            break;
-        case "dataset5":
-                datay = dataset.rental_2011;
-                color = "red";
-                symbol = "circle";
-                name = "2011 Rental Households";
-            break;
-        case "dataset4":
-                datay = dataset.hh_with_children_2011;
-                color = "blue";
-                symbol = "square";
-                name = "2011 Households with Children";
-            break;
         case "dataset3": 
-            datay = dataset.owned_2016;
-            color = "green";
-            symbol = "diamond";
-            name = "2016 Households with Owned Homes";
+            datay1 = dataset.owned_2016;
+            datay2 = dataset.owned_2011;
+            color1 = "green";
+            color2 = "dark blue";
+            symbol1 = "diamond";
+            symbol2 = "circle";
+            name1 = "2016 Households with Owned Homes";
+            name2 = "2011 Households with Owned Homes";
         break;
         case "dataset2":
-            datay = dataset.rental_2016;
-            color = "red";
-            symbol = "circle";
-            name = "2016 Rental Households";
+            datay1 = dataset.rental_2016;
+            datay2 = dataset.rental_2011;
+            color1 = "green";
+            color2 = "dark blue";
+            symbol1 = "diamond";
+            symbol2 = "circle";
+            name1 = "2016 Households with Rental Homes";
+            name2 = "2011 Households with Rental Homes"
         break;
         case "dataset1":
-            datay = dataset.hh_with_children_2016;
-            color = "blue";
-            symbol = "square";
-            name = "2016 Households with Children";
-            header = "2016 Households with Children";
+            datay1 = dataset.hh_with_children_2016;
+            datay2 = dataset.hh_with_children_2011;
+            color1 = "green";
+            color2 = "dark blue";
+            symbol1 = "diamond";
+            symbol2 = "circle";
+            name1 = "2016 Households with Children";
+            name2 = "2011 Households with Children";
     }
-    drawPlot(datay, color, symbol, name)
+    drawPlot(datay1, datay2, color1, color2, symbol1, symbol2, name1, name2)
 }
 
-function drawPlot(datay, color, symbol, name){  
+function drawPlot(datay1, datay2, color1, color2, symbol1, symbol2, name1, name2){  
     Plotly.purge("plot");
-    var trace4 = {
+    var trace5 = {
             x: dataset.ward,
-            y: datay,
-            name: name,
+            y: datay1,
+            name: name1,
             type: "scatter",
             mode: "markers",
             marker: {
-                color: color,
-                symbol: symbol,
+                color: color1,
+                symbol: symbol1,
                 size: 10
                 },
             };
+        var trace6 = {
+            x: dataset.ward,
+            y: datay2,
+            name: name2,
+            type: "scatter",
+            mode: "markers",
+            marker: {
+                color: color2,
+                symbol: symbol2,
+                size: 10
+                },
+            };
+
+
     // Define the plot layout
-    var data = [trace4];
+    var data = [trace5, trace6];
     var layout = {
         xaxis: { title: "Ward" },
         yaxis: { title: "Characteristic"},
